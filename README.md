bda9cb001e4ac27ff6e4b82b3ab1abcf60f6ce25
xavo95 <xavier.perarnau.font@estudiant.upc.edu>
2017-04-02 18:56:53 +0200

    My mirror

---
+++ README.md
@@ -0,0 +1,14 @@
+# MLHPrime Backend
+
+Features:
+
+* Easy end to end encryption
+
+* Simplified tasks
+
+
+```python
+def init(self)
+    self.id = 0
+    self.name = test
+```
---
+++ server/routes/keys.js
@@ -0,0 +1,122 @@
+var db = require('../pg_db_adapter');
+var express = require('express');
+var cryptops = require('../calculate/crypto_ops');
+
+module.exports = function (parentRouter) {
+    var messagesRouter = express.Router();
+
+    messagesRouter
+        .post('/get_id', function (request, response) {
+            var body = request.body;
+            cryptops.returnHash(body['password'], function (res) {
+                var params = [
+                    body['email'],
+                    res
+                ];
+                db.fetchItem('get_user_id', params, function (databasereturned) {
+                    response.json(databasereturned);
+                });
+            });
+        })
+
+        .get('/get_key_by_phone', function (request, response) {
+            var params = [
+                request.query.mobile_phone
+            ];
+
+            db.fetchItem('get_user_id_by_phone', params, function (databasereturned) {
+                var params = [
+                    databasereturned.get_user_id_by_phone
+                ];
+                db.fetchItem('get_user_object', params, function (userObject) {
+                    db.fetchList('list_keys', params, function (keyList) {
+                        userObject.keys = keyList;
+                        response.json(userObject);
+                    });
+                });
+            });
+        })
+
+        .get('/get_key_by_mail', function (request, response) {
+            var params = [
+                request.query.email
+            ];
+
+            db.fetchItem('get_user_id_by_mail', params, function (databasereturned) {
+                var params = [
+                    databasereturned.get_user_id_by_mail
+                ];
+                db.fetchItem('get_user_object', params, function (userObject) {
+                    db.fetchList('list_keys', params, function (keyList) {
+                        userObject.keys = keyList;
+                        response.json(userObject);
+                    });
+                });
+            });
+        })
+
+        .get('/get_key_by_name', function (request, response) {
+            var params = [
+                request.query.full_name
+            ];
+
+            db.fetchItem('get_user_id_by_name', params, function (databasereturned) {
+                var params = [
+                    databasereturned.get_user_id_by_name
+                ];
+                db.fetchItem('get_user_object', params, function (userObject) {
+                    db.fetchList('list_keys', params, function (keyList) {
+                        userObject.keys = keyList;
+                        response.json(userObject);
+                    });
+                });
+            });
+        })
+
+        .get('/remove_key', function (request, response) {
+            var params = [
+                request.query.user_id,
+                request.query.key_name
+            ];
+
+            db.fetchItemAndReturn('remove_key', params, response);
+        })
+
+        .get('/get_keys', function (request, response) {
+            var params = [
+                request.query.id
+            ];
+
+            db.fetchItem('get_user_object', params, function (userObject) {
+                db.fetchList('list_keys', params, function (keyList) {
+                    userObject.keys = keyList;
+                    response.json(userObject);
+                });
+            });
+        })
+
+        .post('/create_key', function (request, response) {
+            var body = request.body;
+            var params = [
+                body['user_id'],
+                body['key'],
+                body['key_name']
+            ];
+            db.fetchItemAndReturn('new_key', params, response);
+        })
+
+        .post('/create_user', function (request, response) {
+            var body = request.body;
+            cryptops.returnHash(body['password'], function (res) {
+                var params = [
+                    body['email'],
+                    res,
+                    body['full_name'],
+                    body['mobile_phone']
+                ];
+                db.fetchItemAndReturn('new_user', params, response);
+            });
+        });
+
+    parentRouter.use('/keys', messagesRouter);
+};
